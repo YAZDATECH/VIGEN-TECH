5a5b734f0c0b18e399d7eaf8625f87673b4edc42
Daniyal-Najarzadeh <daniyal.najarzadeh@gmail.com>
2024-07-21 20:50:39 +0330

    first commit

---
+++ contactform/contactthanks.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body class="contact-body">
+
+	<div id="page-wrap">
+
+		<br /><br />
+		
+			
+		<h1>پیام شما با موفقیت ارسال شد در اسرع آن را بررسی خواهم کرد</h1><br />
+		
+		<p><a href="index.html/#">بازگشت به صفحه اصلی</a></p>
+	
+	</div>
+	
+
+
+</body>
+
+</html>
